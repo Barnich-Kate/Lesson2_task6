ccfd5228ce69b00d40d868c57619e9027ed07689
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-30 21:36:49 +0300

    Create task10.cpp
    Add the solution of the task

---
+++ task10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+int N;
+		cout << "Input number";
+		cin >> N;
+		int i = 1;
+		int summ;
+		int K;
+		while (summ <= N) {
+		summ += i;
+		K = i;
+		i++;
+			
+	}
+		
+	cout << "K=" << K << endl;
+	cout << "Summ=" << summ;
+}
+}
